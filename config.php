f53355600a37cc583df6f070df80555d337dbe80
EC2 Default User <ec2-user@ip-172-31-26-111.us-east-2.compute.internal>
2021-05-14 14:29:39 +0000

    config file updated

--- config.php
+++ config.php
@@ -3,8 +3,9 @@
     $servername = "localhost";
     $username = "first_year";
     $password = "first_year";
+    $dbname = "first_year";
 
-    $conn = mysqli_connect($servername, $username, $password);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
     	if(!$conn){
 		die("Connection failed: ". mysqli_connection_error());
