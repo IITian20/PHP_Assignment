d382b9e4f45bb1bcf05f51d92561830ec80abc1a
EC2 Default User <ec2-user@ip-172-31-26-111.us-east-2.compute.internal>
2021-05-15 10:29:15 +0000

    Logout updated

--- logout.php
+++ logout.php
@@ -3,8 +3,6 @@ session_start();
     unset($_SESSION["Username"]);
     unset($_SESSION["Name"]);
 
-    setcookie("Username","", time() - 3600);
-    setcookie("Password","", time() - 3600);
     header("location: login.php");
 
 
